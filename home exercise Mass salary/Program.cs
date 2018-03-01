52c3f36b52a4a4089f10a145e06e2f91a851c095
tofigf <tofigshf@code.edu.az>
2018-03-01 17:59:34 +0400

    csharp

---
+++ home exercise Mass salary/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_exercise_Mass_salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Mass PercentPrice = new Mass(20,40);
+            Console.WriteLine(PercentPrice.Result);
+            Console.ReadLine();
+
+        }
+    }
+    class Mass
+    {
+        public double Income { get; set; }
+        public double WorkDays { get; set; }
+        private string result { get; set; }
+
+        public string Result
+        {
+            get
+            {
+                this.Calc();
+                return this.result;
+            }
+        }
+        public Mass (double Income, double WorkDays)
+        {
+            this.Income = Income;
+            this.WorkDays = WorkDays;
+        }
+        private void Calc()
+        {
+            if(this.Income * this.WorkDays < 1000)
+            {
+                double r = (this.Income * this.WorkDays) * 18 / 100;
+                this.result = Convert.ToInt32(r) + " azn tutulub";
+            }else if(this.Income * this.WorkDays > 1000)
+                {
+                double r = (this.Income * this.WorkDays) * 22 / 100;
+                this.result = Convert.ToInt32(r) + " azn tutulub ve elave 4% tutlub";
+            }
+
+        }
+
+    }
+}
